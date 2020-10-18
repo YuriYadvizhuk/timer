69c05e4883fd4e34b314f29b7bcb66f3337c4aba
YURY <r1991vm@gmail.com>
2020-10-19 01:04:23 +0300

    текст

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Dec 04, 2020 11:00:00",
+  let birthday = "Dec 04, 2020 00:20:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
